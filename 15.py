dccec64727911b35da48c9cef8fb93159a1a6493
rlarkgus3701 <rlarkgus3701@naver.com>
2021-10-23 00:27:35 +0900

    1장 도전문제 거북이

--- 15.py
+++ 15.py
@@ -1,9 +1,12 @@
 import turtle
 t = turtle.Turtle()
 t.shape("turtle");
+
 t.forward(100);
-t.left(120);
+t.left(90);
 t.forward(100);
-t.left(120);
+t.left(90);
 t.forward(100);
-t.left(120);
+t.left(90);
+t.forward(100);
+t.left(90);
