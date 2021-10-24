8325184149fc446a52f85f46141aa34ee7666204
rlarkgus3701 <rlarkgus3701@naver.com>
2021-10-25 00:09:21 +0900

    3장연습문제

---
+++ 56.py
@@ -0,0 +1,8 @@
+x = int(input("x: 10"))
+y = int(input("y: 20"))
+print("두수의 합: 30", x+y)
+print("두수의 차: -10", x-y)
+print("두수의 곱: 200", x*y)
+print("두수의 평균: 15", (x+y)/2)
+print("큰수: "   , max(x, y))
+print("작은수: ", min(x, y))
---
+++ 57.py
@@ -0,0 +1,4 @@
+r = float(input("r: 10"))
+h = float(input("h: 100"))
+vol = 3.141592*r**2 * h
+print("원기둥의 부피: 31415.92", vol)
