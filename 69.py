5ab1536785a2499bae296fca943b78d58082b2e1
rlarkgus3701 <rlarkgus3701@naver.com>
2021-11-07 18:51:18 +0900

    과제

---
+++ 69.py
@@ -0,0 +1,30 @@
+i = 1
+dan = 1
+while dan <=18:
+    print("%s*%s=%s "%(i,dan,dan*i) , end = " ")
+    i= i+1
+    if i == 8:
+        i = 1
+        print("\n")
+    if i == 1:
+        dan = dan +1
+i = 8
+dan = 1
+while dan <=18:
+    print("%s*%s=%s "%(i,dan,dan*i) , end = " ")
+    i= i+1
+    if i ==15:
+        i = 8
+        print("\n")
+    if i == 8:
+        dan = dan +1
+i = 15
+dan = 1
+while dan <=18:
+    print("%s*%s=%s "%(i,dan,dan*i) , end = " ") 
+    i= i+1
+    if i == 19:
+        i =15
+        print("\n")
+    if i == 15:
+        dan = dan +1   
